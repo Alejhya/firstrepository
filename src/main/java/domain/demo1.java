4325758475404b21f57b2642be28de916072dc2b
1646238 <kalejhya1998@gmail.com>
2021-05-26 20:09:01 +0530

    Motivation

--- src/main/java/domain/demo1.java
+++ src/main/java/domain/demo1.java
@@ -1,4 +1,7 @@
 package domain;
 
 public class demo1 {
+    public static void main(String args[]) {
+        System.out.println("Hello Alekhya....Good things take time!!Trust in your god");
+    }
 }
